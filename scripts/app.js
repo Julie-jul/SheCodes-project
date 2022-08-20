e149ecad09000719cfba869bda0ae63300ec3245
Julie-jul <111383061+Julie-jul@users.noreply.github.com>
2022-08-21 00:43:26 +0300

    Added updated js scripts and styling
    displaying current geo and weather, search engine

--- scripts/app.js
+++ scripts/app.js
@@ -1,43 +1,78 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-// write your code here
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
 
-let city = prompt("Enter a city");
-city = city.toLowerCase();
-
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsius = Math.round(temperature);
-  let farengheit = Math.round((temperature * 9) / 5 + 32);
-
-  alert(
-    `It is currently ${celsius}°C (${farengheit}°F) in ${city} with a humidity of ${humidity}`
-  );
-} else {
-  alert(
-    "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" +
-      city
-  );
+  return `${day} ${hours}:${minutes}`;
 }
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+function showTemperature(response) {
+  let currentTemp = Math.round(response.data.main.temp);
+  let displayTemp = document.querySelector("#temp");
+  displayTemp.innerHTML = `${currentTemp}°C`;
+
+  let humidityLevel = document.querySelector("#humidity");
+  let humidity = response.data.main.humidity;
+  humidityLevel.innerHTML = `Humidity: ${humidity}%`;
+
+  let windSpeed = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  windSpeed.innerHTML = `Wind Speed: ${wind} km/h`;
+
+  let descr = document.querySelector("h3");
+  let description = response.data.weather[0].description;
+  descr.innerHTML = `${description}`;
+
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = response.data.name;
+}
+
+function currentLocation(position) {
+  let apiKey = "5e822b1f123b2af45eff6380d0a24de6";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then(showTemperature);
+}
+
+function currentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}
+
+let locButton = document.querySelector("#loc");
+locButton.addEventListener("click", currentPosition);
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${cityInput.value}`;
+  let apiKey = "5e822b1f123b2af45eff6380d0a24de6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
